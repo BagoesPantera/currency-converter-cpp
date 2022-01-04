a4786506cfca9b2839ad8601657eab80d43b8c4f
BagoesPantera <bagoespantera01@gmail.com>
2022-01-05 05:37:12 +0800

    add with library project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Currency Converter
-This project made with cpp and no library except iostream.
+This project made with cpp and has 2 versions, with a lot of cpp library and wihtout any library except iostream.
--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <stdio.h>
+#include <algorithm> //for transform
+#include <ctime>//for time
+#include <iomanip>//for setprecision (digit after decimal point)
+#include <fstream>//for history
+#include <sstream>//for history
 
 using namespace std;
 
+// Menu
 int pilihan(){
-    int pilihan = 0;
+    int pilihan;
     system("cls");
     cout << "Program Konversi Mata Uang" << endl;
     cout << "1. Konversi" << endl;
@@ -12,181 +19,180 @@ int pilihan(){
     cout << "4. Tutup" << endl;
     cout << "Pilih [1-4] : ";
     cin >> pilihan;
-    if(cin.fail()){
-        cout << "Masukan Angka !";
-        exit(0);
-    }
 
     return pilihan;
 }
- 
-int validate(string *currencySym, int arrLength, char *curr){
-    // Uppercase
-    int i = 0;
-    while(curr[i]!='\0'){ 
-        if(curr[i]>='a' && curr[i]<='z'){ 
-            curr[i]=curr[i]-32; 
-        } 
-        ++i; 
-    }
-    // Validate
-    int result = 9;
-    for(int i = 0; i < arrLength; i++){
-        curr != currencySym[i] ?: result = i;
-    }
-    if (result == 9){
-        cout << "Masukan mata uang tidak valid !";
-        exit(0);
-    }
-    return result;
+// History
+int addHistory(string output, string date){
+    ofstream File;
+    FILE* fout = fopen("history.txt", "a+");
+    fprintf(fout, "%s, Tanggal :  %s", output.c_str(), date.c_str());
+    fclose(fout);
+    return 0;
 }
+int readHistory(){
+    char buf[1000];
+    FILE* fout = fopen("history.txt", "r");
 
-string convertion(){
-    char from[1000],to[1000];
+    if(!fout){
+        cout << endl << "Tidak ada riwayat konversi.";
+        return 0;
+    }
+
+    cout << endl << "Riwayat Konversi" << endl;
+    while (fgets(buf, sizeof buf, fout) != NULL) {
+        cout << buf;
+    }
+    fclose(fout);
+   return 0;
+}
+int removeHistory(){
+    string choice;
+    int rmStatus;
+
+    cout << endl << "Yakin ingin menghapus semua riwayat konversi ? (Y/N) : ";
+    cin >> choice;
+
+    //upper
+    transform(choice.begin(), choice.end(), choice.begin(), ::toupper);
+    
+    if (choice == "Y"){
+        rmStatus = remove("history.txt");
+        if(rmStatus == 0){
+            cout << "Riwayat berhasil dihapus !";
+            return 0;
+        }else{
+            cout << "Riwayat gagal dihapus, coba lagi !";
+            cout << endl << rmStatus;
+            return 0;
+        }
+    }else if (choice == "N"){
+        return 0;
+    }else{
+        cout << "Input tidak valid.";
+        return 0;
+    }
+    
+    return 0;
+}
+int convertion(){
+    //var
+    string from,to;
     string currencySym[7] = {"USD", "IDR", "JPY", "KRW", "RUB", "EUR", "GBP"};
     float currencyNum[7] = {1, 14319.32, 113.84, 1192.3, 74.03, 0.886, 0.755};
     int arrLength = sizeof(currencySym) / sizeof(currencySym[0]);
-    int  amount;
+    int INTfrom = 9 ,INTto = 9, amount;
     float total;
 
-    cout << "Konversi Mata Uang" << endl;
-    cout << "Mata Uang yang Tersedia : ";
+    //menu
+    cout << endl << "Konversi Mata Uang";
+    cout << endl << "Mata Uang yang Tersedia : ";
     for (int i = 0; i < arrLength; i++){
         cout << currencySym[i] << " ";
     }
     cout << endl;
-
-    // From
     cout << "Dari Mata Uang : ";
     cin >> from;
-    int INTfrom = validate(currencySym, arrLength, from);
-
-    // To
     cout << "Ke Mata Uang : ";
     cin >> to;
-    int INTto = validate(currencySym, arrLength, to);
+    cout << "Jumlah Uang (Tanpa ,) : ";
+    cin >> amount;
 
-    // Validate from to
-    if(INTfrom == INTto){   
-        cout << "Mata Uang Tidak Boleh Sama !";
-        exit(0);
+    // UPPERCASE
+    transform(from.begin(), from.end(), from.begin(), ::toupper);
+    transform(to.begin(), to.end(), to.begin(), ::toupper);
+
+    // Convert to integer for EVERYTHING
+    for(int i = 0; i < arrLength; i++){
+        if(from == currencySym[i]){
+            INTfrom = i;
+        }
+        if(to == currencySym[i]){
+            INTto = i;
+        }
     }
 
-    // Amount
-    cout << "Jumlah Uang (Tanpa titik / koma (1.000 = 1000)) : ";
-    cin >> amount;
-    // Validating
-    // Check amount's input
-    if(cin.fail()){
-        cout << "Masukan Angka !";
-        exit(0);
+    // Validating wannabe
+    if(INTfrom == 9 || INTto == 9){
+        cout << "Input Mata Uang Tidak Valid";
+        return 0;
+    }
+    if(INTfrom == INTto){
+        cout << "Mata Uang Tidak Boleh Sama !";
+        return 0;
     }
     if(amount < 1){
         cout << "Jumlah Uang Harus Lebih Dari 0 !";
-        exit(0);
+        return 0;
     }
     
-    total = (currencyNum[INTto]/currencyNum[INTfrom]) * amount;
-    string output = to_string(amount) + " " + currencySym[INTfrom] + " = " + to_string(total) + " " + currencySym[INTto];
+    total = (currencyNum[INTto]/currencyNum[INTfrom]) * amount; //pemikiran random, tiba2 aja kepikiran
 
-    return output;
+    // PREPARING OUTPUT
+    //output
+    stringstream outputString, outputDate;
+    string outputs, outputd;
+
+    outputString << amount << " " << from << " = " << fixed << setprecision(2) << total << " " << to;
+    outputs = outputString.str();
+    
+    // Update Time (kinda sus xD)
+    time_t now = time(0);
+    char* time_now = ctime(&now);// ctime parameter must be pointer
+    outputDate << time_now;
+    outputd = outputDate.str();
+    
+    now = now - (24*60*60);// yesterday
+    char* time_update = ctime(&now);
+
+    
+    // DA REAL OUTPUT RIGHT HERE
+    cout << endl << "Hasil";
+    cout << endl << outputs << endl;
+    cout << "Tanggal di update : " << time_update << endl;
+
+    //addhistory
+    addHistory(outputs,outputd);
+
+    return 0;
 }
-
 int main(){
-
     int choice = pilihan();
-    int id = 0, historySize = 2;
-    string history[historySize];
-
     while (choice){
         switch (choice){
             case 1:
-                {
-                    string output = convertion();
-                    cout << endl << "HASIL" << endl;
-                    cout << output << endl;
-
-                    // Getting user's date
-                    time_t userTime = time(0);
-                    char* time_now = ctime(&userTime);
-                    // Input history
-                    if (id == historySize){
-                        for (int i = 0; i < historySize; i++){
-                            history[i] = history[i + 1];
-                        }
-                        history[id - 1] = output + ", Tanggal " + time_now;
-                    }else{
-                        history[id] = output + ", Tanggal " + time_now;
-                        ++id;
-                    }
-                    // ...and make it yesterday
-                    time_t yesterday = userTime - (24*60*60);
-                    char* time_yesterday = ctime(&yesterday);// ctime parameter must be pointer
-
-                    cout << "Terakhir diupdate tanggal : " << time_yesterday << endl;
-                }
+                convertion();
                 break;
             case 2:
-                cout << endl << "Riwayat Konversi" << endl;
-                if (id == 0){
-                    cout << endl << "Tidak ada riwayat";
-                }else{
-                    for (int i = 0; i < id ; i++){
-                        cout << i + 1 <<". " << history[i];
-                    }
-                }
+                readHistory();
                 break;
             case 3:
-                int choiceDel;
-                cout << endl << "Yakin hapus semua riwayat ? 1. Ya, 2. Tidak : ";
-                cin >> choiceDel;
-
-                switch (choiceDel){
-                    case 1:
-                        id = 0;
-                        cout << "Semua riwayat berhasil dihapus !";
-                        break;
-                    case 2:
-                        cout << "Riwayat tidak di hapus.";
-                        break;
-                    default:
-                        cout << "Tidak valid.";
-                        break;
-                }
+                removeHistory();
                 break;
             case 4:
-                 if (id > 0){
-                    cout << "Menghapus riwayat..." << endl;
-                    cout << "Riwayat terhapus." << endl;
-                }
-                cout << "Terima kasih. Program ditutup.";
+                cout << "Program ditutup...";
                 return 0;
                 break;
             default:
-                cout << "Tidak valid";
-                return 0;
                 break;
         }
-        int lanjut;
-        cout << endl << "Lanjut ? 1.Ya 2.Tidak" << endl;
+
+        string lanjut;
+        cout << endl << "Lanjutkan ? (Y/N) : ";
         cin >> lanjut;
-        switch (lanjut){
-            case 1:
-                choice = pilihan();
-                break;
-            case 2:
-                if (id > 0){
-                    cout << "Menghapus riwayat..." << endl;
-                    cout << "Riwayat terhapus." << endl;
-                }
-                cout << "Terima kasih. Program ditutup.";
-                return 0;
-                break;
-            default:
-                cout << "Masukan tidak valid, program ditutup !";
-                return 0;
-                break;  
+
+        transform(lanjut.begin(), lanjut.end(), lanjut.begin(), ::toupper);
+
+        if (lanjut == "Y"){
+            choice = pilihan();
+        }else if (lanjut == "N"){
+            cout << "Program ditutup";
+            return 0;
+        }else{
+            cout << "Input tidak valid.";
+            return 0;
         }
     }
+
     return 0;
 }
